90d3692b8872614cbffc6ef62423f24ebef9af57
Kevin Rutherford <kevin@rutherford-software.com>
2013-12-12 21:21:40 +0000

    Basic clean-up, suitable for wikipedia

--- coffee-shop/java/src/CoffeeShop.java
+++ coffee-shop/java/src/CoffeeShop.java
@@ -1,94 +1,88 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
- 
-// Flyweight object interface
-interface CoffeeOrder {
-    public void serveCoffee(CoffeeOrderContext context);
+
+// Instances of CoffeeFlavour will be the Flyweights
+class CoffeeFlavour {
+	private final String name;
+
+	CoffeeFlavour(String newFlavor) {
+		this.name = newFlavor;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
 }
- 
-// ConcreteFlyweight object that creates ConcreteFlyweight 
-class CoffeeFlavor implements CoffeeOrder {
-    private final String flavor;
- 
-    public CoffeeFlavor(String newFlavor) {
-        this.flavor = newFlavor;
-    }
- 
-    public String getFlavor() {
-        return this.flavor;
-    }
- 
-    public void serveCoffee(CoffeeOrderContext context) {
-        System.out.println("Serving Coffee flavor " + flavor + " to table number " + context.getTable());
-    }
+
+// Menu acts as a factory and cache for CoffeeFlavour flyweight objects
+class Menu {
+	private Map<String, CoffeeFlavour> flavours = new HashMap<String, CoffeeFlavour>();
+
+	CoffeeFlavour lookup(String flavorName) {
+		if (!flavours.containsKey(flavorName))
+			flavours.put(flavorName, new CoffeeFlavour(flavorName));
+		return flavours.get(flavorName);
+	}
+
+	int totalCoffeeFlavoursMade() {
+		return flavours.size();
+	}
 }
- 
-class CoffeeOrderContext {
-   private final int tableNumber;
- 
-   public CoffeeOrderContext(int tableNumber) {
-       this.tableNumber = tableNumber;
-   }
- 
-   public int getTable() {
-       return this.tableNumber;
-   }
+
+class Order {
+	private final int tableNumber;
+	private final CoffeeFlavour flavour;
+
+	Order(int tableNumber, CoffeeFlavour flavor) {
+		this.tableNumber = tableNumber;
+		this.flavour = flavor;
+	}
+
+	void serve() {
+		System.out.println("Serving " + flavour + " to table " + tableNumber);
+	}
 }
- 
-//FlyweightFactory object
-class CoffeeFlavorFactory {
-    private Map<String, CoffeeFlavor> flavors = new HashMap<String, CoffeeFlavor>();
- 
-    public CoffeeFlavor getCoffeeFlavor(String flavorName) {
-        CoffeeFlavor flavor = flavors.get(flavorName);
-        if (flavor == null) {
-            flavor = new CoffeeFlavor(flavorName);
-            flavors.put(flavorName, flavor);
-        }
-        return flavor;
-    }
- 
-    public int getTotalCoffeeFlavorsMade() {
-        return flavors.size();
-    }
-}
- 
+
 class CoffeeShop {
-   /** The flavors ordered. */
-   private static CoffeeFlavor[] flavors = new CoffeeFlavor[100];
-   /** The tables for the orders. */
-   private static CoffeeOrderContext[] tables = new CoffeeOrderContext[100];
-   private static int ordersMade = 0;
-   private static CoffeeFlavorFactory flavorFactory;
- 
-   public static void takeOrders(String flavorIn, int table) {
-       flavors[ordersMade] = flavorFactory.getCoffeeFlavor(flavorIn);
-       tables[ordersMade++] = new CoffeeOrderContext(table);
-   }
- 
-   public static void main(String[] args) {
-       flavorFactory = new CoffeeFlavorFactory();
- 
-       takeOrders("Cappuccino", 2);
-       takeOrders("Cappuccino", 2);
-       takeOrders("Frappe", 1);
-       takeOrders("Frappe", 1);
-       takeOrders("Espresso", 1);
-       takeOrders("Frappe", 897);
-       takeOrders("Cappuccino", 97);
-       takeOrders("Cappuccino", 97);
-       takeOrders("Frappe", 3);
-       takeOrders("Espresso", 3);
-       takeOrders("Cappuccino", 3);
-       takeOrders("Espresso", 96);
-       takeOrders("Frappe", 552);
-       takeOrders("Cappuccino", 121);
-       takeOrders("Espresso", 121);
- 
-       for (int i = 0; i < ordersMade; ++i) {
-           flavors[i].serveCoffee(tables[i]);
-       }
-       System.out.println(" ");
-       System.out.println("total CoffeeFlavor objects made: " +  flavorFactory.getTotalCoffeeFlavorsMade());
-   }
+	private final List<Order> orders = new ArrayList<Order>();
+	private final Menu menu = new Menu();
+
+	void takeOrder(String flavourName, int table) {
+		CoffeeFlavour flavour = menu.lookup(flavourName);
+		Order order = new Order(table, flavour);
+		orders.add(order);
+	}
+	
+	void service() {
+		for (Order order : orders)
+			order.serve();		
+	}
+	
+	String report() {
+		return "\ntotal CoffeeFlavour objects made: "
+				+ menu.totalCoffeeFlavoursMade();
+	}
+
+	public static void main(String[] args) {
+		CoffeeShop shop = new CoffeeShop();
+
+		shop.takeOrder("Cappuccino", 2);
+		shop.takeOrder("Frappe", 1);
+		shop.takeOrder("Espresso", 1);
+		shop.takeOrder("Frappe", 897);
+		shop.takeOrder("Cappuccino", 97);
+		shop.takeOrder("Frappe", 3);
+		shop.takeOrder("Espresso", 3);
+		shop.takeOrder("Cappuccino", 3);
+		shop.takeOrder("Espresso", 96);
+		shop.takeOrder("Frappe", 552);
+		shop.takeOrder("Cappuccino", 121);
+		shop.takeOrder("Espresso", 121);
+
+		shop.service();
+		System.out.println(shop.report());
+	}
 }
