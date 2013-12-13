496047075bfb226818e0cd8264c84f7e568dec6f
Kevin Rutherford <kevin@rutherford-software.com>
2013-12-13 14:35:57 +0000

    Added a lot more test coverage

--- coffee-shop/java/src/coffee_shop/CoffeeShopTest.java
+++ coffee-shop/java/src/coffee_shop/CoffeeShopTest.java
@@ -1,23 +1,54 @@
 package coffee_shop;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CoffeeShopTest implements CoffeeShopListener {
 
 	private int flavoursMade = 0;
+	private CoffeeShop shop;
+	private String serviceReport = "";
+	
+	@Before
+	public void setUp() {
+		shop = new CoffeeShop();
+		shop.addListener(this);
+	}
+
+	@Test
+	public void coffeeFlavourConstructionIsReported() {
+		shop.takeOrder("Cappuccino", 97);
+		assertEquals(1, flavoursMade);
+	}
+
+	@Test
+	public void ordersServedAreReported() {
+		shop.takeOrder("Cappuccino", 97);
+		shop.service();
+		assertEquals("[97, Cappuccino]", serviceReport);
+	}
 
 	@Test
 	public void coffeeFlavoursAreReused() {
-		CoffeeShop shop = new CoffeeShop();
-		shop.addListener(this);
 		shop.takeOrder("Cappuccino", 2);
 		shop.takeOrder("Cappuccino", 97);
 		assertEquals(1, flavoursMade);
 	}
 
+	@Test
+	public void ordersAreServedInOrder() {
+		shop.takeOrder("Cappuccino", 2);
+		shop.takeOrder("Mocha", 97);
+		shop.service();
+		assertEquals("[2, Cappuccino][97, Mocha]", serviceReport);
+	}
+
 	@Override
-	public void orderServed(int tableNumber, String flavour) { }
+	public void orderServed(int tableNumber, String flavour) {
+		serviceReport += "[" + tableNumber + ", " + flavour + "]";
+	}
 
 	@Override
 	public void newCoffeeFlavourMade(String flavorName) {
