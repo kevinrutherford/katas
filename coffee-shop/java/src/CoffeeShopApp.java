d140b8bfdb10d59902b4d746231283f6ac45e0fe
Kevin Rutherford <kevin@rutherford-software.com>
2013-12-13 11:36:45 +0000

    Extracted main() to a new app and added the first unit test

---
+++ coffee-shop/java/src/CoffeeShopApp.java
@@ -0,0 +1,24 @@
+
+class CoffeeShopApp {
+
+	public static void main(String[] args) {
+		CoffeeShop shop = new CoffeeShop();
+
+		shop.takeOrder("Cappuccino", 2);
+		shop.takeOrder("Frappe", 1);
+		shop.takeOrder("Espresso", 1);
+		shop.takeOrder("Frappe", 897);
+		shop.takeOrder("Cappuccino", 97);
+		shop.takeOrder("Frappe", 3);
+		shop.takeOrder("Espresso", 3);
+		shop.takeOrder("Cappuccino", 3);
+		shop.takeOrder("Espresso", 96);
+		shop.takeOrder("Frappe", 552);
+		shop.takeOrder("Cappuccino", 121);
+		shop.takeOrder("Espresso", 121);
+
+		shop.service();
+		System.out.println(shop.report());
+	}
+	
+}
---
+++ coffee-shop/java/src/CoffeeShopTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CoffeeShopTest {
+
+	@Test
+	public void onlyThreeCoffeeFlavoursAreMade() {
+		CoffeeShop shop = new CoffeeShop();
+
+		shop.takeOrder("Cappuccino", 2);
+		shop.takeOrder("Frappe", 1);
+		shop.takeOrder("Espresso", 1);
+		shop.takeOrder("Frappe", 897);
+		shop.takeOrder("Cappuccino", 97);
+		shop.takeOrder("Frappe", 3);
+		shop.takeOrder("Espresso", 3);
+		shop.takeOrder("Cappuccino", 3);
+		shop.takeOrder("Espresso", 96);
+		shop.takeOrder("Frappe", 552);
+		shop.takeOrder("Cappuccino", 121);
+		shop.takeOrder("Espresso", 121);
+
+		shop.service();
+		assertEquals(3, shop.numberOfFlavoursMade());
+	}
+
+}
